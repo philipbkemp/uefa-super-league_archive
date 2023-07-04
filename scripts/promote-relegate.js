8e7262d6d94b7668f2b9d965efcc3cbca42ab152
Phil Kemp <boondock.dragon@gmail.com>
2023-07-04 15:34:13 +0200

    1911-12

---
+++ scripts/promote-relegate.js
@@ -0,0 +1,32 @@
+divisions = document.querySelectorAll("table.division");
+newCount = [];
+newCount[0] = 0;
+relegation = false;
+promotion = false;
+divisions.forEach(function(d,dIndex){
+    newCount[dIndex+1] = 0;
+	rows = d.querySelectorAll("tbody tr");
+	rows.forEach(function(r,rIndex){
+        if ( r.classList.contains("promoted") ) {
+            newCount[dIndex-1]++;
+            promotion = true;
+        } else if ( r.classList.contains("relegated") ) {
+            newCount[dIndex+1]++;
+            relegation = true;
+        } else if ( ! r.classList.contains("removed") ) {
+            newCount[dIndex]++;
+        }
+    });
+});
+divNames = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+for ( i=0 ; i!==newCount.length ; i++ ) {
+    if ( newCount[i] > 35 && newCount[i] !== 40 ) {
+        console.log("Division " + divNames[i] + " should relegate " + (newCount[i]-35) + " team(s)");
+        break;
+    } else if ( newCount[i] <= 35 && newCount[i+1] !== 0 && newCount[i] !== 0 ) {
+        console.log("Division " + divNames[i+1] + " should promote " + (40-newCount[i]) + " team(s)");
+        break;
+    } else {
+        console.log("Division " + divNames[i] + " has " + newCount[i] + " team(s)");
+    }
+}
